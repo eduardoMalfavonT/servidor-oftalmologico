29c8b8dab3942b33377eb730deaac8bafd012ed1
Metoosky00 <malfavontalaveraj@hotmail.com>
2020-05-06 12:52:03 -0500

    creacion de rutas
    Creo las rutas especificas para poder utilizar el servidor en una pagina web

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const clienteController= require('../controllers/clienteController');
+const productoController= require('../controllers/productoController');
+const ventasController= require('../controllers/ventasController');
+
+module.exports = function () {
+  /*-----------------------------------------------------------Rutas-------------------------------------------------------- */
+  /*---------------------------------------------------Rutas para cleintes------------------------------------------------ */
+  //Crear un nuevo cliente
+  router.post("/cliente/nuevoCliente", clienteController.nuevoCliente);
+  //Mostrar todos los clientes
+  router.get("/cliente", clienteController.mostrarClientes);
+  //Mostrar un solo cliente
+  router.get("/cliente/:idCliente", clienteController.mostrarCliente);
+  //Actualizar cliente
+  router.put("/cliente/:idCliente", clienteController.actualizarCliente);
+  //Eliminar un cliente
+  router.delete("/cliente/:idCliente", clienteController.eliminarCliente);
+  /*-------------------------------------------------Rutas para productos----------------------------------------------- */
+  //Crear nuevo producto
+  router.post("/producto/nuevoProducto", productoController.nuevoProducto);
+  //Mostrar todos los productos
+  router.get("/productos", productoController.mostrarProductos);
+  //Mostrar un solo producto
+  router.get("/producto/:idProducto", productoController.mostrarProducto);
+  //Actualizar un producto
+  router.put("/producto/:idProducto", productoController.actualizarProducto);
+  //Eliminar un producto
+  router.delete("/producto/:idProducto", productoController.eliminarProducto);
+  /*---------------------------------------------------Rutas para ventas--------------------------------------------------*/
+  //Crear una nueva venta
+  router.post("/venta/nuevaVenta", ventasController.nuevaVenta);
+  //Mostrar todas las ventas
+  router.get("/ventas", ventasController.mostrarVentas);
+  //Mostrar una venta
+  router.get("/venta/idVenta", ventasController.mostrarVenta);
+  //Actualizar una venta
+  router.put("/venta/idVenta", ventasController.actualizarVenta);
+  //Eliminar una venta
+  router.delete("/venta/idVenta", ventasController.eliminarVenta);
+  return router;
+};
