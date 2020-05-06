46c0bf96dce3919c6ceb710bc21b6e9407106a60
Metoosky00 <malfavontalaveraj@hotmail.com>
2020-05-06 12:31:19 -0500

    Inicializo el servidor
    Inicializo el servidor con todas las dependencias necesarias y conectandolo con la base de datos de mongo

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = require("./routes/index");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/oftalmologico", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const App = express();
+App.use(bodyParser.json());
+App.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+App.use(cors());
+App.use("/", routes());
+App.use(express.static("uploads"));
+App.listen(5000);
